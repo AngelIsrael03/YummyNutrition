ec2436e6dc4d43ace4fdb8b20218c85823a22b2e
Frida Torres <fridamichelletorres148@gmail.com>
2026-01-12 21:01:55 -0600

    Mich cambios

--- app/src/main/java/com/example/yummynutrition/ui/theme/screens/SplashScreen.kt
+++ app/src/main/java/com/example/yummynutrition/ui/theme/screens/SplashScreen.kt
@@ -1,65 +1,38 @@
 package com.example.yummynutrition.ui.theme.screens
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.yummynutrition.R
+import com.example.yummynutrition.ui.theme.md_theme_dark_background
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
-    onNavigateNext: () -> Unit
+    onFinish: () -> Unit
 ) {
-    val rotation = rememberInfiniteTransition().animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(4000, easing = LinearEasing)
-        )
-    )
-
     LaunchedEffect(Unit) {
-        delay(3000)
-        onNavigateNext()
+        delay(1800)
+        onFinish()
     }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .background(md_theme_dark_background),
         contentAlignment = Alignment.Center
     ) {
-
-        // Texto girando
-        Box(
-            modifier = Modifier
-                .size(260.dp)
-                .rotate(rotation.value),
-            contentAlignment = Alignment.TopCenter
-        ) {
-            Text(
-                text = "YUMMI  •  NUTRITION  •  YUMMI  •  NUTRITION",
-                color = MaterialTheme.colorScheme.primary,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
-        // Logo
         Image(
-            painter = painterResource(id = R.drawable.yummi),
-            contentDescription = "Yummi Logo",
-            modifier = Modifier.size(160.dp)
+            painter = painterResource(R.drawable.yummi),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.85f)   // 🔥 OCUPA CASI TODA LA PANTALLA
+                .aspectRatio(1f)       // mantiene proporción tipo logo
         )
     }
 }
